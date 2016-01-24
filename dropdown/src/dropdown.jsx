fa296b36185384165b7cd76d04f44752cf52f50e
Paolo Ibarra <jpibarra1130@gmail.com>
2016-01-24 15:45:05 +0800

    Tutorial for handling events between parent-child components
    Signed-off-by: Paolo Ibarra <jpibarra1130@gmail.com>

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -1,11 +1,28 @@
 var React = require('react');
 var Button = require('./button');
-// var List = require('./list');
+var ListItem = require('./list-item');
 
 module.exports = React.createClass({
-render: function() {
-  return <div className='dropdown'>
-    <Button className='btn-default' title={this.props.title} subTitleClassName='caret' subTitle='29' />
-  </div>
-}
+  handleClick: function() {
+    
+  },  
+  render: function() {
+    var list = this.props.items.map(function(item) {
+      return <ListItem item={item}/>
+
+    });
+
+    return <div className='dropdown'>
+      <Button 
+        whenClicked={this.handleClick} 
+        className='btn-default' 
+        title={this.props.title} 
+        subTitleClassName='caret' 
+        subTitle='29' />
+      
+      <ul>
+      {list}
+      </ul>
+    </div>
+  }
 });
