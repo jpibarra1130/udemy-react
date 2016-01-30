7eaccf5c3cbda666d087f8039b0a3cb891a4f074
Paolo Ibarra <jpibarra1130@gmail.com>
2016-01-30 22:13:56 +0800

    Added handler for clicking the element in the dropdown

--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -4,23 +4,36 @@ var ListItem = require('./list-item');
 
 module.exports = React.createClass({
   handleClick: function() {
-    
-  },  
+    this.setState({open: !this.state.open});
+  },
+  getInitialState: function() {
+    // gets run once when the function gets rendered
+    return { open: false }
+  },
+  handleItemClicked: function(item) {
+    this.setState({
+      open: false,
+      itemTitle: item
+    })
+  },
   render: function() {
     var list = this.props.items.map(function(item) {
-      return <ListItem item={item}/>
+      return <ListItem 
+                item={item} 
+                whenItemClicked={this.handleItemClicked} 
+                className={this.state.itemTitle == item ? 'active' : ''}/>
 
-    });
+    }.bind(this));
 
     return <div className='dropdown'>
       <Button 
         whenClicked={this.handleClick} 
         className='btn-default' 
-        title={this.props.title} 
+        title={this.state.itemTitle || this.props.title } 
         subTitleClassName='caret' 
         subTitle='29' />
       
-      <ul>
+      <ul className={'dropdown-menu ' + (this.state.open ? "show" : "")}>
       {list}
       </ul>
     </div>
